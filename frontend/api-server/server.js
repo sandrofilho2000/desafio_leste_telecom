ce997d017654acbff0bb7dbe6efbc3b1ec09deec
Sandro Filho <75636911+sandrofilho2000@users.noreply.github.com>
2024-07-04 02:18:26 -0300

    fix: fix server.js

--- frontend/api-server/server.js
+++ frontend/api-server/server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-const multer = require('multer');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const FormData = require('form-data');
-const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,8 +12,6 @@ const API_URL = process.env.API_URL;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-const upload = multer({ dest: 'uploads/' });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -35,38 +30,17 @@ app.get('/api/contacts', async (req, res) => {
   }
 });
 
-app.post('/api/create_contact', upload.single('avatar'), async (req, res) => {
+app.post('/api/create_contact', async (req, res) => {
   const formData = req.body;
-  const avatarFile = req.file;
 
   try {
-    const formDataToSend = new FormData();
-    formDataToSend.append('first_name', formData.first_name);
-    formDataToSend.append('last_name', formData.last_name);
-    formDataToSend.append('email', formData.email);
-    formDataToSend.append('birthdate', formData.birthdate);
-    formDataToSend.append('gender', formData.gender);
-    formDataToSend.append('language', formData.language);
-
-    if (avatarFile) {
-      const avatarData = fs.createReadStream(avatarFile.path);
-      formDataToSend.append('avatar', avatarData, {
-        filename: avatarFile.originalname,
-        contentType: avatarFile.mimetype,
-      });
-    }
-
     const headers = {
-      'Content-Type': 'multipart/form-data',
       Authorization: `Api-Key ${API_KEY}`,
-      ...formDataToSend.getHeaders(),
     };
 
-    const response = await axios.post(
-      `${API_URL}/create_contact/`,
-      formDataToSend,
-      { headers }
-    );
+    const response = await axios.post(`${API_URL}/create_contact/`, formData, {
+      headers,
+    });
 
     res.status(200).json({ message: 'Contact created successfully' });
   } catch (error) {
@@ -75,48 +49,27 @@ app.post('/api/create_contact', upload.single('avatar'), async (req, res) => {
   }
 });
 
-app.post(
-  '/api/update_contact/:contact_id',
-  upload.single('avatar'),
-  async (req, res) => {
-    const { contact_id } = req.params;
-    const formData = req.body;
-    const avatarFile = req.file;
+app.post('/api/update_contact/:contact_id', async (req, res) => {
+  const { contact_id } = req.params;
+  const formData = req.body;
 
-    const formDataToSend = new FormData();
-    formDataToSend.append('first_name', formData.first_name);
-    formDataToSend.append('last_name', formData.last_name);
-    formDataToSend.append('email', formData.email);
-    formDataToSend.append('birthdate', formData.birthdate);
-    formDataToSend.append('gender', formData.gender);
-    formDataToSend.append('language', formData.language);
+  try {
+    const headers = {
+      Authorization: `Api-Key ${API_KEY}`,
+    };
 
-    if (avatarFile) {
-      const avatarData = fs.createReadStream(avatarFile.path);
-      formDataToSend.append('avatar', avatarData, {
-        filename: avatarFile.originalname,
-        contentType: avatarFile.mimetype,
-      });
-    }
-    try {
-      const response = await axios.post(
-        `${API_URL}/update_contact/${contact_id}/`,
-        formDataToSend,
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-            Authorization: `Api-Key ${API_KEY}`,
-          },
-        }
-      );
+    const response = await axios.post(
+      `${API_URL}/update_contact/${contact_id}/`,
+      formData,
+      { headers }
+    );
 
-      res.status(200).json({ message: 'Contact updated successfully' });
-    } catch (error) {
-      console.error('Req failed!:', error.message);
-      res.status(500).json({ error: 'Req failed!' });
-    }
+    res.status(200).json({ message: 'Contact updated successfully' });
+  } catch (error) {
+    console.error('Req failed!:', error.message);
+    res.status(500).json({ error: 'Req failed!' });
   }
-);
+});
 
 app.delete('/api/delete_contact/:contact_id/', async (req, res) => {
   try {
